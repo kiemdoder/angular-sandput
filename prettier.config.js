c1192a27a1fc8309959e0ed9ef3802518b7ea028
Roelf de Kock <Roelf.deKock@redstor.com>
2019-07-18 15:53:04 +0200

    added prettier config

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	arrowParens: 'avoid', // other option 'avoid'
+	bracketSpacing: true,
+	printWidth: 140, //default = 80
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none', // other options `es5` or `all`
+	useTabs: false
+};
+
+/* 
+Remove the following from tslint.json to not conflict with Prettier:
+- comment-format
+- curly
+- eofline
+- import-spacing
+- indent
+- max-line-length
+- no-trailing-whitespace
+- one-line
+- quotemark
+- semicolon
+- typedef-whitespace
+- angular-whitespace
+*/
