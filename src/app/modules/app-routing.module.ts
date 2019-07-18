ddcabf944219d96ded2ad7f109bc01bc5bcafd0b
Roelf de Kock <Roelf.deKock@redstor.com>
2019-07-18 15:22:08 +0200

    goeters

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from '../components/heroes/heroes.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { HeroDetailComponent } from 'src/app/components/hero-detail/hero-detail.component';
+import { LinegraphComponent } from '../components/linegraph/linegraph.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'heroes', component: HeroesComponent},
   {path: 'detail/:id', component: HeroDetailComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'graph', component: LinegraphComponent},
 ];
 
 @NgModule({
